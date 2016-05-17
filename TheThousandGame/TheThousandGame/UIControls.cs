58de215949752cd2bcfd8a2a26443c4e7bd6fc79
Кирилл Кутепов <k.kutepov@ya.ru>
2016-05-17 21:08:54 +0300

    Added initial UIControls.cs as place for further UI control methods

---
+++ TheThousandGame/TheThousandGame/UIControls.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheThousandGame
+{
+    public partial class Form1
+    {
+        public void ee()
+        {
+        }
+    }
+}
